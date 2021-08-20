8c29cf9405a96ef73925b233da3072166dc10349
Xander Madsen <xander.madsen@sciencelogic.com>
2021-08-20 10:34:03 -0400

    Try http socket addresses

--- frontend/plugins/socket.ts
+++ frontend/plugins/socket.ts
@@ -3,8 +3,8 @@ import io from 'socket.io-client'
 
 const socketAddress =
   process.env.NODE_ENV === `development`
-    ? `ws://localhost:4200`
-    : `ws://starfish.cool:4200`
+    ? `http://localhost:4200`
+    : `http://starfish.cool:4200`
 c.log(
   `initializing frontend socket connection at`,
   socketAddress,
