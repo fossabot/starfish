706815656c7bb1b623f95e3336974437e470590f
Xander Madsen <xander.madsen@sciencelogic.com>
2021-08-20 17:44:16 -0400

    update socket.ts with starfish.cool

--- frontend/plugins/socket.ts
+++ frontend/plugins/socket.ts
@@ -4,7 +4,7 @@ import io from 'socket.io-client'
 const socketAddress =
   process.env.NODE_ENV === `development`
     ? `http://localhost:4200`
-    : `http://game:4200`
+    : `http://starfish.cool:4200`
 c.log(
   `initializing frontend socket connection at`,
   socketAddress,
