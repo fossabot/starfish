86cf1cfed968011e80ea2a25de68f0090e054557
Xander Madsen <xander.madsen@sciencelogic.com>
2021-08-26 11:59:21 -0400

    update docker-compose-prod and nginx config and game server to use cert

--- game/src/server/io.ts
+++ game/src/server/io.ts
@@ -1,16 +1,23 @@
 import c from '../../../common/dist'
-import { createServer } from 'http'
 import { Server, Socket } from 'socket.io'
+import fs from 'fs'
 
 import frontendEvents from './events/frontend'
 import discordEvents from './events/discord'
 import generalEvents from './events/general'
 import crewEvents from './events/crew'
 import itemEvents from './events/items'
+import https from 'https'
 
-const httpServer = createServer()
+const privateKey = fs.readFileSync(process.env.SERVER_PRIVATE_KEY, 'utf8')
+const certificate = fs.readFileSync(process.env.SERVER_CERTIFICATE, 'utf8')
+const credentials = {
+    key: privateKey, 
+    cert: certificate, 
+}
+const httpsServer = https.createServer(credentials)
 const io = new Server<IOClientEvents, IOServerEvents>(
-  httpServer,
+  httpsServer,
   {
     cors: {
       origin: `*`,
@@ -29,7 +36,7 @@ io.on(
   },
 )
 
-httpServer.listen(4200)
+httpsServer.listen(4200)
 c.log(`io server listening on port 4200`)
 
 export default io
