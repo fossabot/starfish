68fa27211bd29a02b174ca4be3df4d42d9cc53f8
Xander Madsen <xander.madsen@sciencelogic.com>
2021-09-20 10:34:28 -0400

    track prettierrc.json, update common/dist with prettier

--- common/dist/cycle.d.ts
+++ common/dist/cycle.d.ts
@@ -1,3 +1,3 @@
-export declare function decycle(object: any): any;
-export declare function retrocycle($: any): any;
+export declare function decycle(object: any): any
+export declare function retrocycle($: any): any
 //# sourceMappingURL=cycle.d.ts.map
