fc918b97e95ce3f59bb8f17fb870c4135fd6527b
Jasper Stephenson <jasperstephenson@gmail.com>
2021-09-20 12:39:00 +0900

    Formatting and local dev url fixes for frontend

--- frontend/plugins/socket.ts
+++ frontend/plugins/socket.ts
@@ -3,19 +3,18 @@ import io from 'socket.io-client'
 
 const socketAddress =
   process.env.NODE_ENV === `development`
-    ? `https://localhost:4200`
+    ? process.env.IS_DOCKER
+      ? `https://localhost:4200`
+      : `http://localhost:4200`
     : `https://www.starfish.cool:4200`
-// c.log(
-//   `initializing frontend socket connection at`,
-//   socketAddress,
-// )
+c.log(
+  `initializing frontend socket connection at`,
+  socketAddress,
+)
 const socket = io(socketAddress, {
   secure: true,
   rejectUnauthorized: false,
 })
-
-// // test
-// socket.emit(`hello`)
 
 export default function ({ app }, inject) {
   inject(`socket`, socket)
