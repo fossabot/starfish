cb98696498c09d71acc5cf70282a0352c1db1bbc
Jasper Stephenson <jasperstephenson@gmail.com>
2021-09-23 23:02:31 +0900

    Beautifying left bar

--- frontend/components/ship/LeftBar.vue
+++ frontend/components/ship/LeftBar.vue
@@ -2,26 +2,36 @@
   <nav class="leftbar">
     <div class="placeholder"></div>
     <div class="actualbar flexcolumn flexcenter">
-      <nuxt-link class="icon marbot" to="/"
+      <nuxt-link class="logoholder" to="/"
         ><img src="/images/logo.svg" class="logo"
       /></nuxt-link>
+
+      <hr />
+
       <div
         class="icon guildicon marbot"
         v-for="s in shipsBasics"
-        :class="{ active: ship && ship.id === s.id }"
         @click="shipSelected(s.id)"
+        :class="{ active: ship && ship.id === s.id }"
       >
-        <div class="underimage flexcenter">
-          {{ c.acronym(s.name).substring(0, 3) }}
-        </div>
+        <div class="activeicon"></div>
+
         <div
-          class="image"
-          :style="{
-            'background-image': s.guildIcon
-              ? `url('${s.guildIcon}')`
-              : '',
-          }"
-        ></div>
+          class="iconholder"
+          :class="{ active: ship && ship.id === s.id }"
+        >
+          <div class="underimage flexcenter">
+            {{ c.acronym(s.name).substring(0, 3) }}
+          </div>
+          <div
+            class="image"
+            :style="{
+              'background-image': s.guildIcon
+                ? `url('${s.guildIcon}')`
+                : '',
+            }"
+          ></div>
+        </div>
       </div>
     </div>
   </nav>
@@ -57,8 +67,15 @@ export default Vue.extend({
 
 <style lang="scss" scoped>
 .leftbar {
-  --bar-width: 50px;
-  --icon-width: 35px;
+  --bar-width: 65px;
+  --icon-width: 45px;
+}
+
+hr {
+  margin: 0.7em auto;
+  border: 0;
+  border-bottom: 1.5px solid rgba(255, 255, 255, 0.2);
+  width: 50%;
 }
 
 .placeholder {
@@ -70,8 +87,8 @@ export default Vue.extend({
 .actualbar {
   background: rgba(0, 0, 0, 0.4);
   align-items: center;
+  padding: 1.5em 0;
   justify-content: flex-start;
-  padding: 1.5em;
   width: var(--bar-width);
   height: 100vh;
   flex-grow: 0;
@@ -79,11 +96,7 @@ export default Vue.extend({
   position: fixed;
   top: 0;
   left: 0;
-  border-right: 1px solid rgba(255, 255, 255, 0.1);
-
-  & > .icon {
-    width: var(--icon-width);
-  }
+  border-right: 1px solid rgba(165, 65, 65, 0.1);
 }
 
 .logo {
@@ -92,39 +105,87 @@ export default Vue.extend({
 
 .guildicon {
   position: relative;
+  width: var(--bar-width);
+  display: flex;
+  justify-content: center;
 
-  .underimage,
-  .image {
-    top: 0;
-    left: 0;
-    overflow: hidden;
-    border-radius: calc(var(--icon-width) / 2);
-    width: 100%;
-    height: var(--icon-width);
-  }
-  .underimage {
-    position: relative;
-    font-weight: bold;
-  }
-  .image {
+  .activeicon {
+    --activewidth: 3px;
     position: absolute;
-    box-shadow: 0 0 0 1px rgba(255, 255, 255, 0.3);
-    background-size: cover;
+    top: 50%;
+    left: 0;
+    width: var(--activewidth);
+    background: white;
+    height: 0%;
+    border-top-right-radius: 5px;
+    border-bottom-right-radius: 5px;
 
-    transition: box-shadow 0.2s ease-in-out;
+    left: calc(-1 * var(--activewidth));
+    transition: left 0.2s ease-in-out, top 0.2s ease-in-out,
+      height 0.2s ease-in-out;
   }
 
   &.active {
+    .underimage {
+      background: rgba(255, 255, 255, 0.3);
+      border-radius: calc(var(--icon-width) / 3.5);
+    }
     .image {
-      box-shadow: 0 0 0 1.5px white;
+      // box-shadow: 0 0 0 1.5px white;
+      box-shadow: none;
+      border-radius: calc(var(--icon-width) / 3.5);
     }
   }
 
   &:hover {
     cursor: pointer;
 
+    .activeicon {
+      left: 0;
+      top: 30%;
+      height: 40%;
+    }
+
+    .underimage {
+      background: rgba(255, 255, 255, 0.4);
+    }
+  }
+
+  &.active {
+    .activeicon {
+      left: 0;
+      top: 10%;
+      height: 80%;
+    }
+  }
+
+  .iconholder {
+    position: relative;
+    width: var(--icon-width);
+    height: var(--icon-width);
+
+    .underimage,
     .image {
-      box-shadow: 0 0 0 2px rgba(255, 255, 255, 1);
+      top: 0;
+      left: 0;
+      overflow: hidden;
+      border-radius: calc(var(--icon-width) / 2);
+      width: 100%;
+      height: 100%;
+      transition: background 0.2s ease-in-out,
+        box-shadow 0.2s ease-in-out,
+        border-radius 0.2s ease-in-out;
+    }
+    .underimage {
+      position: relative;
+      font-weight: bold;
+      top: 0.037em;
+      background: rgba(255, 255, 255, 0.2);
+    }
+    .image {
+      position: absolute;
+      // box-shadow: 0 0 0 1px rgba(255, 255, 255, 0.3);
+      background-size: cover;
     }
   }
 }
