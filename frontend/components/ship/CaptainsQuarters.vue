44083aa556a2fa258cebbbb835091dd989c69d08
Jasper Stephenson <jasperstephenson@gmail.com>
2021-09-24 20:47:49 +0900

    (keep forgetting to turn off the orders pane for 1 player)

--- frontend/components/ship/CaptainsQuarters.vue
+++ frontend/components/ship/CaptainsQuarters.vue
@@ -354,7 +354,7 @@ export default Vue.extend({
     show(): boolean {
       return (
         this.ship &&
-        // this.ship.crewMembers.length > 1 &&
+        this.ship.crewMembers.length > 1 &&
         this.captain &&
         (!this.ship.shownPanels ||
           this.ship.shownPanels.includes(
