6c697b421785d6dab2735e63bc6154fd2b18b0e4
Xander Madsen <xander.madsen@sciencelogic.com>
2021-10-02 15:23:13 -0400

    Improve Links message from bot

--- discord/src/discordClient/commands/Link.ts
+++ discord/src/discordClient/commands/Link.ts
@@ -9,15 +9,18 @@ export class LinkCommand implements Command {
   commandNames = [`link`, `url`]
 
   getHelpMessage(commandPrefix: string): string {
-    return `\`${commandPrefix}${this.commandNames[0]}\` - Get a link to your ship's console.`
+    return `\`${commandPrefix}${this.commandNames[0]}\` - Get some useful Starfish links..`
   }
 
   async run(context: CommandContext): Promise<void> {
     await context.reply({
       embeds: [
-        new MessageEmbed({
-          description: `[Ship console](${c.frontendUrl})\n\n[Bot invite](${c.discordBotInviteUrl})\n\n[Support server](${c.supportServerLink})`,
-        }),
+        new MessageEmbed()
+          .setColor(`#FF9F49`)
+          .setTitle(`Starfish Links`)
+          .setDescription(
+            `:desktop: [Ship console](${c.frontendUrl})\n\n:incoming_envelope: [Bot invite](${c.discordBotInviteUrl})\n\n:information_source: [Support server](${c.supportServerLink})`,
+          ),
       ],
     })
   }
