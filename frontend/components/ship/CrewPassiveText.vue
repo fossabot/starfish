9b3fc31784d3384803830e8b452557cab469a1a3
Jasper Stephenson <jasperstephenson@gmail.com>
2021-10-03 11:03:37 +0900

    new frontend components

---
+++ frontend/components/ship/CrewPassiveText.vue
@@ -0,0 +1,57 @@
+<template>
+  <span>
+    <span
+      :class="{
+        success: (passive.intensity || 0) > 0,
+        warning: (passive.intensity || 0) < 0,
+      }"
+    >
+      {{ c.crewPassives[passive.id].description(passive) }}
+    </span>
+    <span
+      class="sub nowrap"
+      v-if="passive.data && passive.data.source"
+    >
+      ({{
+        passive.data.source.speciesId
+          ? `${c.capitalize(
+              c.species[passive.data.source.speciesId]
+                .singular,
+            )} species`
+          : passive.data.source.planetName
+          ? `Planet ${passive.data.source.planetName}`
+          : passive.data.source.item
+          ? `${
+              c.items[passive.data.source.item.type][
+                passive.data.source.item.id
+              ].displayName
+            }`
+          : passive.data.source.chassisId
+          ? `${
+              c.items.chassis[passive.data.source.chassisId]
+                .displayName
+            }`
+          : passive.data.source
+      }})
+    </span>
+  </span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import c from '../../../common/dist'
+import { mapState } from 'vuex'
+
+export default Vue.extend({
+  props: { passive: { required: true } },
+  data() {
+    return { c }
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+  methods: {},
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ frontend/components/ship/planet/GuildRecruit.vue
@@ -0,0 +1,124 @@
+<template>
+  <div
+    class="guildrecruit panesection"
+    v-if="
+      ship &&
+      planet &&
+      planet.guildId &&
+      c.guilds[planet.guildId] &&
+      ship.guildId !== planet.guildId
+    "
+  >
+    <div class="panesubhead">
+      Join the
+      <span :style="{ color: guild.color }"
+        >{{ guild.name }} Guild</span
+      >!
+    </div>
+
+    <div v-if="isCaptain">
+      <div class="sub">
+        Captain, you can join your ship into the
+        {{ guild.name }} Guild<span v-if="changePrice">
+          for {{ changePrice }} credits</span
+        >. You can change guilds at guild homeworlds, but it
+        will cost credits to switch.
+      </div>
+      <div>
+        <button
+          @click="joinGuild"
+          :disabled="
+            !!(
+              changePrice &&
+              ship.commonCredits < changePrice
+            )
+          "
+          class="button big martopsmall"
+        >
+          Join Guild: 💳{{
+            c.numberWithCommas(changePrice)
+          }}
+        </button>
+      </div>
+    </div>
+
+    <div v-else class="sub">
+      The captain can join your ship into the
+      {{ guild.name }} Guild<span v-if="changePrice">
+        for {{ changePrice }} credits</span
+      >.
+    </div>
+
+    <div class="benefits martop">
+      <div class="bold">
+        {{ guild.name }} Guild members get:
+      </div>
+
+      <ul v-for="p in guild.passives" class="small success">
+        <li><ShipPassiveText :passive="p" /></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import c from '../../../../common/dist'
+import { mapState } from 'vuex'
+
+export default Vue.extend({
+  data() {
+    return { c }
+  },
+  computed: {
+    ...mapState(['ship', 'userId', 'crewMember']),
+    changePrice(): number {
+      return c.getGuildChangePrice(this.ship)
+    },
+    isCaptain(): boolean {
+      return this.ship?.captain === this.userId
+    },
+    planet(): PlanetStub {
+      return this.ship.planet
+    },
+    guild(): BaseGuildData {
+      return c.guilds[
+        (this.ship?.planet as any)?.guildId as GuildId
+      ]
+    },
+  },
+  watch: {},
+  mounted() {},
+  methods: {
+    joinGuild() {
+      if (
+        this.ship.guildId &&
+        !confirm('Are you sure you want to change guilds?')
+      )
+        return
+
+      this.$store.commit('updateShip', {
+        guildId: this.guild.id,
+      })
+      ;(this as any).$socket?.emit(
+        'ship:joinGuild',
+        this.ship.id,
+        this.crewMember?.id,
+        this.ship?.planet?.guildId,
+        (res: IOResponse<CrewMemberStub>) => {
+          if ('error' in res) {
+            this.$store.dispatch('notifications/notify', {
+              text: res.error,
+              type: 'error',
+            })
+            console.log(res.error)
+            return
+          }
+        },
+      )
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
