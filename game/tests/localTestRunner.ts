d12d943dd422b820cc136ab37d3e9a18713bfb2e
Jasper Stephenson <jasperstephenson@gmail.com>
2021-10-20 11:12:18 +0900

    Bump mocha timeout from 2 to 10 seconds

--- game/tests/localTestRunner.ts
+++ game/tests/localTestRunner.ts
@@ -1,7 +1,7 @@
 import { exec } from 'child_process'
 
 exec(
-  `TS_NODE_PROJECT="tsconfig.test.json" mocha --require ts-node/register --exit 'tests/index.ts'`,
+  `TS_NODE_PROJECT="tsconfig.test.json" mocha --timeout 10000 --require ts-node/register --exit 'tests/index.ts'`,
   undefined,
   (error, stdout, stderr) => {
     if (error) console.log(error)
