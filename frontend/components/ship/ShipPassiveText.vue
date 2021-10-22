335db9155d0a824c9678c0cefe58d990943f3cd8
Jasper Stephenson <jasperstephenson@gmail.com>
2021-10-22 17:44:12 +0900

    (more zone stuff)

--- frontend/components/ship/ShipPassiveText.vue
+++ frontend/components/ship/ShipPassiveText.vue
@@ -16,7 +16,7 @@
       class="sub nowrap"
       v-if="passive.data && passive.data.source"
     >
-      ({{
+      {{
         passive.data.source.guildId
           ? `${c.capitalize(
               c.guilds[passive.data.source.guildId].name,
@@ -27,7 +27,9 @@
                 .singular,
             )} species`
           : passive.data.source.planetName
-          ? `Planet ${passive.data.source.planetName}`
+          ? `${passive.data.source.planetName}`
+          : passive.data.source.zoneName
+          ? `${passive.data.source.zoneName}`
           : passive.data.source.item
           ? `${
               c.items[passive.data.source.item.type][
@@ -40,7 +42,7 @@
                 .displayName
             }`
           : passive.data.source
-      }})
+      }}
     </span>
   </span>
 </template>
