0c403e60e1506c68b24de2f1e080f8a46b24b4eb
Xander Madsen <xander.madsen@sciencelogic.com>
2021-10-25 12:37:45 -0400

    update spec

--- cypress/integration/1-auth/homepage.spec.js
+++ cypress/integration/1-auth/homepage.spec.js
@@ -29,19 +29,19 @@ describe(`Homepage`, () => {
 
   links.forEach((link) => {
     it(`"${link.text}" link correctly navigates to ${hostname}/${link.href}`, () => {
-      cy.visit(`https://www.starfish.cool`)
+      cy.visit(hostname)
 
       cy.get(`a`).filter(`:contains(${link.text})`).click()
       cy.url().should(`eq`, `${hostname}/${link.href}`)
     })
   })
 })
-describe(`Discord Login`, () => {
+describe.only(`Discord Login`, () => {
   it(`Log In takes user to Discord login page`, () => {
-    cy.clearLocalStorage()
-    cy.visit(`https://www.starfish.cool`)
+    cy.visit(hostname)
 
     cy.get(`a`).filter(`:contains(Log In)`).click()
+
     cy.url().should(
       `contain`,
       `https://discord.com/oauth2/authorize`,
@@ -54,21 +54,51 @@ describe(`Discord Login`, () => {
       value: `${Cypress.env(`DISCORD_CLIENT_ID`)}`,
     },
   ]
+
   urlParams.forEach((param) => {
     it(`URL contains ${param.name}`, () => {
-      cy.url().should(`contain`, param.value)
+      cy.url().should(
+        `contain`,
+        `${param.name}=${param.value}`,
+      )
     })
   })
-  it(`should authorize with Discord`, () => {
-    expect(discordUser, `discordUser was set`).to.be.a(
-      `string`,
-    ).and.not.be.empty
-    expect(discordPass, `discordPass was set`).to.be.a(
-      `string`,
-    ).and.not.be.empty
 
-    cy.get(`[name=email]`).type(discordUser)
-    cy.get(`[name=password]`).type(discordPass)
-    cy.get(`[type=Submit]`).click()
+  it(`should authorize with Discord`, () => {
+    cy.get(`body`).then((body) => {
+      if (
+        body.find(`a[class^="logoutLink"]`).length === 0
+      ) {
+        expect(discordUser, `discordUser was set`).to.be.a(
+          `string`,
+        ).and.not.be.empty
+        expect(discordPass, `discordPass was set`).to.be.a(
+          `string`,
+        ).and.not.be.empty
+
+        cy.get(`[name=email]`).type(discordUser)
+        cy.get(`[name=password]`).type(discordPass)
+
+        cy.get(`[type=Submit]`).click() // Login
+        cy.get(`button`)
+          .filter(`:contains(Authorize)`)
+          .click()
+      } else {
+        cy.get(`button`)
+          .filter(`:contains(Authorize)`)
+          .click()
+      }
+
+      cy.url().should(`eq`, `${hostname}/s`)
+    })
+  })
+
+  it(`should log out`, () => {
+    cy.visit(`${hostname}/s`)
+
+    cy.get(`img[alt="log out button"]`).click()
+    cy.url().should(`eq`, `${hostname}/`, {
+      timeout: 10000,
+    })
   })
 })
