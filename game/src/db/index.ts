6c75ee928eda279946b63e67c5339e04991ecc06
Xander Madsen <xander.madsen@gmail.com>
2022-01-29 15:16:19 -0500

    Feature/update workflows (#543)

--- game/src/db/index.ts
+++ game/src/db/index.ts
@@ -1,19 +1,19 @@
+import { exec } from 'child_process'
 import { config as dotEnvConfig } from 'dotenv'
-import isDocker from 'is-docker'
 import * as fs from 'fs'
-import * as cache from './models/cache'
-import * as ship from './models/ship'
-import * as attackRemnant from './models/attackRemnant'
-import * as planet from './models/planet'
-import * as zone from './models/zone'
-import * as gameSettings from './models/gameSettings'
-import * as game from './models/game'
-dotEnvConfig()
-
-import c from '../../../common/dist'
+import isDocker from 'is-docker'
 import mongoose from 'mongoose'
 import path from 'path'
-import { exec } from 'child_process'
+import c from '../../../common/dist'
+import * as attackRemnant from './models/attackRemnant'
+import * as cache from './models/cache'
+import * as game from './models/game'
+import * as gameSettings from './models/gameSettings'
+import * as planet from './models/planet'
+import * as ship from './models/ship'
+import * as zone from './models/zone'
+dotEnvConfig()
+
 export const db = {
   cache,
   ship,
@@ -38,7 +38,7 @@ try {
     .trim()
 } catch (e) {
   mongoUsername =
-    process.env.NODE_ENV === `staging`
+    process.env.NODE_ENV === `ci`
       ? `testuser`
       : (process.env.MONGODB_ADMINUSERNAME as string)
 }
@@ -48,12 +48,12 @@ try {
     .trim()
 } catch (e) {
   mongoPassword =
-    process.env.NODE_ENV === `staging`
+    process.env.NODE_ENV === `ci`
       ? `testpassword`
       : (process.env.MONGODB_ADMINPASSWORD as string)
 }
 
-databaseName = process.env.NODE_ENV === `staging` ? `starfish-test` : `starfish`
+databaseName = process.env.NODE_ENV === `ci` ? `starfish-test` : `starfish`
 // c.log({ databaseName, mongoUsername, mongoPassword })
 
 const defaultMongoOptions: GameDbOptions = {
